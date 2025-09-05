39f2d383ca5def8b73b41f23229cdf2c44590cc6
Muhammad Anas <anasahmedofficial786@gmail.com>
2025-09-05 23:56:15 +0500

    Practice of Youtube Code Snippet

---
+++ 01-youtube-tutorial/Practical/Snippet01Demo/Program.cs
@@ -0,0 +1,42 @@
+﻿// This is a simple code for demonstrating static method call.
+// This is a Comment
+/* This is also a comment */
+// static void SayHello(string name)
+// {
+//     Console.WriteLine($"Hello {name}!");
+// }
+// SayHello("Anas");
+// SayHello("Saad");
+
+
+// // Example:
+// string fruit = "Apple";
+// Console.WriteLine($"I like {fruit}");
+// // Output: I like Apple
+
+
+// // Taking Input from Console
+// static void SayHelloByConsole(string name)
+// {
+//     Console.WriteLine($"Hello {name}!");
+// }
+// SayHelloByConsole(Console.ReadLine());
+
+
+using System;
+class Greeter
+{
+    public void SayHello(string name)
+    {
+        Console.WriteLine($"Hello {name}!");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        var g = new Greeter();
+        g.SayHello("Anas");
+        g.SayHello("Saad");
+    }
+}
---
+++ 01-youtube-tutorial/Practical/StaticDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+static class MathHelper
+{   
+    // Static Class with Static Member and Static Method.
+    public static double Pi = 3.14159;
+
+    public static int Square(int x) => x * x;
+}
+
+class Greeter
+{
+    // Static Method.
+    public static void SayHello(string name) => Console.WriteLine($"Hello {name}!");
+}
+
+class Counter
+{
+    // Static Member
+    public static int Count = 0;
+
+    // Constructer.
+    public Counter()
+    {
+        Count++; // Increase for all Instance.
+    }
+
+}
+
+class Config
+{
+    public static string Setting;
+
+    static Config()
+    {
+        Setting = "Initialized only once!";
+        Console.WriteLine("Static Constructor Called.");
+    } 
+}
+
+class Program
+{
+    static void Main()
+    {
+        // For Class MathHelper.
+        Console.WriteLine(MathHelper.Pi);          // 3.14159
+        Console.WriteLine(MathHelper.Square(5));   // 25
+
+        // For Class Greeter.
+        Greeter.SayHello("Anas");
+
+        // For class Counter
+        new Counter();
+        new Counter();
+        new Counter();
+        new Counter();
+        Console.WriteLine(Counter.Count);  // 4 (shared, not per object)
+
+        // Usage
+        // For Class Config
+        Console.WriteLine(Config.Setting);
+
+    }
+}
