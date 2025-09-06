6fed570b7e165bd581b66e1aa542e22d1fecd0cc
Muhammad Anas <anasahmedofficial786@gmail.com>
2025-09-07 04:50:24 +0500

    Practice

---
+++ 01-youtube-tutorial/Practical/Snippet05Demo/Program.cs
@@ -0,0 +1,104 @@
+﻿// Snippet 05 => Access Modifiers
+// public class MyClass
+// private
+// protected
+// internal
+// file
+
+// // Example for Public Access Modifiers
+// public class Car
+// {
+//     public void Drive() => Console.WriteLine("Driving");
+// }
+// class Program
+// {
+//     static void Main()
+//     {
+//         var action = new Car();
+//         action.Drive();
+//     }
+// }
+
+// Example for Private Access Modifiers
+// class Engine
+// {
+//     private void Ignite() => Console.WriteLine("Ignited");
+
+//     public void InClassCall()
+//     {
+//         // ✅ Allowed: private method can be called inside the same class
+//         Ignite();
+//     }
+// }
+// class Program
+// {
+//     static void Main()
+//     {
+//         Engine engine = new Engine();
+//         // engine.Ignite(); ❌ // Not allowed, because Ignite() is private
+//         // ✅ Allowed, calls private Ignite() internally
+//         engine.InClassCall();
+//     }
+// }
+
+// Example for Protected Access Modifiers
+// public class MyClass
+// {
+//     protected void MyMethod() => Console.WriteLine("Hello, I am from Protected method.");
+// }
+// public class Child : MyClass
+// {
+//     public void Call() => MyMethod(); // ✅ allowed
+// }
+// class Program
+// {
+//     static void Main()
+//     {
+//         Child child = new Child();
+//         child.Call();
+//     }
+// }
+
+// Example for Internal Access Modifiers
+// internal class Helper { }
+
+// Example for file Access Modifiers
+// file class FileScopedHelper { }
+
+using System;
+
+public class MyClass
+{
+    public void PublicMethod() => Console.WriteLine("Public");
+    private void PrivateMethod() => Console.WriteLine("Private");
+    protected void ProtectedMethod() => Console.WriteLine("Protected");
+    internal void InternalMethod() => Console.WriteLine("Internal");
+
+    // Only work in C# (11+)
+    // file class FileHelper { public void Print() => Console.WriteLine("File Scoped"); }
+
+    public void InClassCallProtectedMethod()
+    {
+        ProtectedMethod();
+    }
+}
+class Child : MyClass
+{
+    public void Test()
+    {
+        // this.PrivateMethod();   // ❌ not allowed
+        this.ProtectedMethod();    // ✅ allowed in derived class
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        var obj = new MyClass();
+        obj.PublicMethod();    // ✅ allowed
+        obj.InternalMethod();  // ✅ allowed (same assembly)
+        obj.InClassCallProtectedMethod(); // Calling from within class
+        // obj.ProtectedMethod(); // ❌ not allowed
+        // obj.PrivateMethod();   // ❌ not allowed
+    }
+}
---
+++ 01-youtube-tutorial/Practical/Snippet06Demo/Program.cs
@@ -0,0 +1,59 @@
+﻿//Snippet06
+// public class MyClass
+// {
+//     protected void MyMethod()
+//     {
+//         Console.WriteLine("Hello");
+//     }
+// }
+
+
+// 01
+// public class Car
+// {
+//     public string Model;
+//     public void Drive() => Console.WriteLine($"{Model} is driving");
+// }
+// class Program
+// {
+//     static void Main()
+//     {
+//         var car = new Car { Model = "Honda" };
+//         car.Drive();  // Honda is driving
+
+//     }
+// }
+
+
+// 02
+// public class Animal { public void Eat() => Console.WriteLine("Eating"); }
+// public class Dog : Animal { public void Bark() => Console.WriteLine("Bark"); }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         var dog = new Dog();
+//         dog.Bark();
+//         dog.Eat();
+//     }
+// }
+
+
+//03
+// interface IMovable { void Move(); }
+// public class Car : IMovable { public void Move() => Console.WriteLine("Car moves"); }
+// class Program
+// {
+//     static void Main()
+//     {
+//         // 1. Call via class reference => Class reference (Car)
+//         Car car = new Car();
+//         car.Move();
+
+//         // 2. Call via interface reference => Interface reference (IMovable)
+//         IMovable moveable = new Car();
+//         moveable.Move();
+//     }
+// }
+
